90ea7afdc4b291c05aa71084c8d44ec4b934d1d8
hemchan <108014789+hemuchanz@users.noreply.github.com>
2022-07-15 01:38:50 +0900

    20220714

--- App.jsx
+++ App.jsx
@@ -1,10 +1,16 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang={true}>World</Hello>
+      <Hello style={{fontSize: 16,backgroundColor:'white',color:'black'}}>World</Hello>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line react/style-prop-object */}
       <StatusBar style="auto" />
     </View>
   );
