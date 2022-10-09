5fb1cb6614870a5a27fa066476716ac47b4134a2
hemchan <108014789+hemuchanz@users.noreply.github.com>
2022-10-10 02:31:49 +0900

    20221010

--- App.jsx
+++ App.jsx
@@ -1,17 +1,57 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Hello bang={true}>World</Hello>
-      <Hello style={{fontSize: 16,backgroundColor:'white',color:'black'}}>World</Hello>
-      <Text>Open up App.js to start working on your app!</Text>
-      {/* eslint-disable-next-line react/style-prop-object */}
-      <StatusBar style="auto" />
+      {/* app bar */}
+      <View style={styles.appbar}>
+        <View style={styles.appbarInner}>
+          <Text style={styles.appbarTitle}>Memo App</Text>
+          <Text style={styles.appbarRight}>ログアウト</Text>
+        </View>
+      </View>
+
+      {/* Memo List */}
+      <View>
+
+        <View style={styles.meoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.meoListItemDate}>2022年10月09日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View style={styles.meoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.meoListItemDate}>2022年10月09日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View style={styles.meoListItem}>
+          <View>
+            <Text style={styles.memoListItemTitle}>買い物リスト</Text>
+            <Text style={styles.meoListItemDate}>2022年10月09日</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+
+      </View>
+
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
+      </View>
     </View>
   );
 }
@@ -19,8 +59,70 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F0F4F8',
   },
+  appbar: {
+    width: '100%',
+    height: 90,
+    backgroundColor: '#467FD3',
+    justifyContent: 'flex-end',
+  },
+  appbarInner: {
+    alignItems: 'center',
+  },
+
+  appbarRight: {
+    position: 'absolute',
+    right: 19,
+    bottom: 12,
+    color: 'rgba(255, 255, 255, 0.8)'
+  },
+  appbarTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: '#FFFF',
+    fontWeight: 'bold'
+  },
+  meoListItem: {
+    backgroundColor: '#ffffff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: 'center',
+    borderBottomWidth: 0.5,
+    borderColor: 'rgba(0, 0, 0, 0.15)'
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  meoListItemDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#848484',
+  },
+  circleButton: {
+    backgroundColor: '#467FD3',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    right: 30,
+    bottom: 30,
+    shadowColor: '#000000',
+    shadowOffset: {width:0,height:8},
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: '#ffffff',
+    fontSize: 40,
+    lineHeight: 40,
+  }
+
 });
